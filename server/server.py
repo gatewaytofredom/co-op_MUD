bf2b9b5e336483c82043669459e0a31c0f98ee95
gatewaytofreedom <bobdosgames@gmail.com>
2018-08-17 21:05:39 -0500

    server can now bind to port
    the rest might work

--- server/server.py
+++ server/server.py
@@ -6,8 +6,11 @@ class Server():
 
     def __init__(self,port):
         self.port = port
-        self.host = socket.gethostbyname()
+        self.host = socket.gethostbyname(socket.gethostname())
         print(self.host)
+
+        if self.port == '':
+            self.port = 6624
 
         try:
             self.s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
@@ -15,6 +18,8 @@ class Server():
 
             #change to number of players + 1
             self.s.listen(10)
+            print('Server bind to port sucsesful')
+
         except:
             print('unable to start server. possible port conflict')
 
@@ -23,33 +28,30 @@ class Server():
         #Sqlite database probs
         pass
 
-    def conenction_listener(self):
-
-        t = Thread(target= self.listener_loop)
+    def create_conection_thread(self):
+        t = Thread(target= self.wait_for_connection)
         t.daemon = True
         t.start()
 
-    def listener_loop(self):
+    def wait_for_connection(self):
         while True:
            con = clientsocket,addr = self.s.accept()
 
 
-    def handle_client(self, clientsocket, addr):
+    def client_connection(self, clientsocket, addr):
         #make not while true
         while True:
            clientsocket.send("AAA".encode())
            clientsocket.recv(1024).decode()
 
+
+server = Server(port=6624)
+
+
+
+
         
-
-
-
-
-    
-
-
-if __name__ == "__main__":
-    server = Server(sys.argv[1])
+        
 
 
 
