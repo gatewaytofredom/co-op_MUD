3156b1925262fae3a39f41ca11fc841e70a7cf7a
Nic Hodlofski <fred-simons47@hotmail.com>
2018-08-30 22:18:43 -0500

    Add engine template
    I am a god of programming

---
+++ server/mudengine.py
@@ -0,0 +1,11 @@
+
+class Engine:
+    def player_command(self, player, command):
+        pass
+    
+
+class Player:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        #login player
